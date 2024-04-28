6323c0c7c155962ba766cb432814f3e663200ff5
Pedro Beltran <PEDROALEXANDER.BELTRAN.CONTRACTOR.CIDENTITY@BBVA.COM>
2024-04-28 01:47:39 -0500

    Day 1 - Corrections in article components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import CrossIcon from "./components/icons/CrossIcon";
-import MoonIcon from "./components/icons/MoonIcon";
+import CrossIcon from './components/icons/CrossIcon';
+import MoonIcon from './components/icons/MoonIcon';
 
 const App = () => {
   return (
@@ -50,37 +50,41 @@ const App = () => {
           </form>
         </header>
         <main className="container mx-auto mt-8 px-4 ">
-          <div className="rounded-md bg-white [&>article]:p-4 [&>article]:flex [&>article]:gap-4 [&>article]:border-b [&>article]:items-center">
+          <div className="rounded-md bg-white [&>article]:flex [&>article]:items-center [&>article]:gap-4 [&>article]:border-b [&>article]:p-4">
             <article>
-              <button className="inline-block h-5 w-5 rounded-full border-2 flex-none"></button>
-              <p className="text-gray-600 grow">
+              <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+              <p className="grow text-gray-600">
                 Complete online JavaScript course in BluuWeb
               </p>
               <CrossIcon />
             </article>
             <article>
-              <button className="flex-none inline-block h-5 w-5 rounded-full border-2"></button>
-              <p>Complete online JavaScript course in BluuWeb</p>
+              <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+              <p className="grow text-gray-600">
+                Complete online JavaScript course in BluuWeb
+              </p>
               <CrossIcon />
             </article>
             <article>
-              <button className="flex-none inline-block h-5 w-5 rounded-full border-2"></button>
-              <p>Complete online JavaScript course in BluuWeb</p>
+              <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+              <p className="grow text-gray-600">
+                Complete online JavaScript course in BluuWeb
+              </p>
               <CrossIcon />
-            </article>
-            <section className="p-4 text-gray-400 flex justify-between">
+            </article>            
+            <section className="flex justify-between p-4 text-gray-400">
               <span>5 items left</span>
               <button>Clear Completed</button>
             </section>
           </div>
 
-          <section className="container mx-auto p-4 bg-white mt-8 rounded flex justify-center gap-4">
+          <section className="container mx-auto mt-8 flex justify-center gap-4 rounded bg-white p-4">
             <button className="text-blue-600">All</button>
             <button className="hover:text-blue-600">Active</button>
             <button className="hover:text-blue-600">Completed</button>
           </section>
 
-          <p className="text-center mt-8 pb-8">Drag and drop to reorder list</p>
+          <p className="mt-8 pb-8 text-center">Drag and drop to reorder list</p>
         </main>
       </div>
     </>
